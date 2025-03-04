765d420b9e5e456313c8a866a4d49f90514ea418
Jorge Cáceres <jcaceres@gemastic.com>
2025-03-03 22:28:07 -0500

    JWCC - Se agrega campo state_sale, solo se tuvo que crear la BD desde cero, son errores no de desarrollo sino de infraestructura, por algún motivo la BD queda corrompida

--- efs_delivery/models/res_partner.py
+++ efs_delivery/models/res_partner.py
@@ -3,9 +3,10 @@ from odoo import models, fields
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
+    state_sale = fields.Char(string='Estado de Venta')
     customer_category = fields.Selection([
         ('premium', 'Premium'),
         ('wholesale', 'Mayorista'),
         ('retail', 'Minorista')
     ], string='Categoría de Cliente', default='retail', required=True)
-    #state_sale = fields.Char(string="estado de venta")
+    
